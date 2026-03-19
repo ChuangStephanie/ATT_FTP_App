6ce09dcb47a46189913528945243c52d3d02ac52
ChuangStephanie <stephanie231951@yahoo.com>
2026-03-19 09:58:30 -0500

    server starts

--- server/server.js
+++ server/server.js
@@ -6,9 +6,11 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const url = "https://interfacetst.genco.com:7004/reflashIB";
+const testUrl = "https://interfacetst.genco.com:7004/reflashIB";
+const url = "https://interface.genco.com:7004/reflashIB";
 
 app.post("/upload-xml", async(req, res) => {
+    try {
     const xmldata = req.body.xml;
 
     const parsed = await xml2js.parseStringPromise(xmldata);
@@ -18,7 +20,7 @@ app.post("/upload-xml", async(req, res) => {
      const payload = {
             Reflash: {
                 Header: {
-                    Account: "Group O"
+                    Account: "Techcess Sollutions"
                 },
                 Unit: {
                     Esn: unit.ESN[0],
@@ -30,4 +32,28 @@ app.post("/upload-xml", async(req, res) => {
                 }
             }
         };
+
+        const response = await axios.post(
+            testUrl, payload,
+            {
+                auth: {
+                    username: "reflashUser",
+                    password: "reflash2fsc^tst"
+                },
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+
+        res.json({
+            success: true,
+            apiResponse: response.data
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
 })
+
+app.listen(3001, () => console.log("Server running on port 3001"));
