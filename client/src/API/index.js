d1f32def03b41ad2b6db868bd9e980b1f2bad21c
ChuangStephanie <stephanie231951@yahoo.com>
2026-03-19 16:28:53 -0500

    add component and api call

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,9 +1,27 @@
 const baseURL = "http://localhost:3001";
 
-const uploadFile = async(file) => {
-    if (!file) {
-        alert("No file uploaded");
-        return;
+const uploadFile = async (file) => {
+  if (!file) {
+    alert("No file uploaded");
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await fetch(`${baseURL}/upload-xml`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+        throw new Error(`Server error: ${response.status}`);
     }
 
-}
+    return await response.json();
+  } catch (error) {
+    console.error(`Error uploading:`, error);
+    throw error;
+  }
+};
