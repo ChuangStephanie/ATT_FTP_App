5f1887cc4e2bc291497364a8a1452be77df0639f
ChuangStephanie <stephanie231951@yahoo.com>
2026-03-19 14:03:31 -0500

    changed code to accept files

---
+++ client/src/API/index.js
@@ -0,0 +1,9 @@
+const baseURL = "http://localhost:3001";
+
+const uploadFile = async(file) => {
+    if (!file) {
+        alert("No file uploaded");
+        return;
+    }
+
+}
--- server/server.js
+++ server/server.js
@@ -1,59 +1,60 @@
 const express = require("express");
+const multer = require("multer");
 const fs = require("fs");
 const xml2js = require("xml2js");
 const axios = require("axios");
 
 const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 app.use(express.json());
 
 const testUrl = "https://interfacetst.genco.com:7004/reflashIB";
 const url = "https://interface.genco.com:7004/reflashIB";
 
-app.post("/upload-xml", async(req, res) => {
-    try {
-    const xmldata = req.body.xml;
+app.post("/upload-xml", upload.single("file"), async (req, res) => {
+  try {
+    const xmldata = req.file.buffer.toString();
 
     const parsed = await xml2js.parseStringPromise(xmldata);
-
     const unit = parsed.RELASH.UNIT[0];
 
-     const payload = {
-            Reflash: {
-                Header: {
-                    Account: "Techcess Sollutions"
-                },
-                Unit: {
-                    Esn: unit.ESN[0],
-                    Timestamp: unit.TIMESTAMP[0],
-                    Result: unit.RESULT[0],
-                    SoftwareName: unit.SOFTWARE_NAME[0],
-                    SoftwareVer: unit.SOFTWARE_VER[0],
-                    OemName: unit.OEM_NAME[0]
-                }
-            }
-        };
+    const payload = {
+      Reflash: {
+        Header: {
+          Account: "Techcess Sollutions",
+        },
+        Unit: {
+          Esn: unit.ESN[0],
+          Timestamp: unit.TIMESTAMP[0],
+          Result: unit.RESULT[0],
+          SoftwareName: unit.SOFTWARE_NAME[0],
+          SoftwareVer: unit.SOFTWARE_VER[0],
+          OemName: unit.OEM_NAME[0],
+        },
+      },
+    };
 
-        const response = await axios.post(
-            testUrl, payload,
-            {
-                auth: {
-                    username: "reflashUser",
-                    password: "reflash2fsc^tst"
-                },
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }
-        );
+    console.log(payload);
+    console.log("xml parsed");
 
-        res.json({
-            success: true,
-            apiResponse: response.data
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: err.message });
-    }
-})
+    const response = await axios.post(testUrl, payload, {
+      auth: {
+        username: "reflashUser",
+        password: "reflash2fsc^tst",
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    res.json({
+      success: true,
+      apiResponse: response.data,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
 
 app.listen(3001, () => console.log("Server running on port 3001"));
