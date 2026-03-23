cb48affcf30a19a87d5e8934741036b1d15ae392
ChuangStephanie <stephanie231951@yahoo.com>
2026-03-23 12:01:57 -0500

    added upload and submit buttons

--- client/src/API/index.js
+++ client/src/API/index.js
@@ -1,6 +1,6 @@
 const baseURL = "http://localhost:3001";
 
-const uploadFile = async (file) => {
+export const uploadXML = async (file) => {
   if (!file) {
     alert("No file uploaded");
     return;
--- client/src/Components/Upload.jsx
+++ client/src/Components/Upload.jsx
@@ -8,8 +8,66 @@ import {
   colors,
 } from "@mui/material";
 import { CloudUploadRounded } from "@mui/icons-material";
+import { uploadXML } from "../API/index.js";
 
 export default function Upload() {
+  const [upload, setUpload] = useState(null);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarColor, setSnackbarColor] = useState("#49c758");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  // shows snackbar
+  const showSnackbar = (message, color) => {
+    setSnackbarMessage(message);
+    if (color) {
+      setSnackbarColor(color);
+    } else {
+      setSnackbarColor("#49c758");
+    }
+    setSnackbarOpen(true);
+  };
+
+  // closes snackbar
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+
+  const handleFiles = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) return;
+
+    if (e.target.name === "upload-file") {
+      setUpload(file);
+      console.log("File uploaded");
+      console.log(upload);
+      showSnackbar("File uploaded");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // show error if no file uploaded
+    if (!upload) {
+      showSnackbar("Please upload required files", "red");
+      return;
+    }
+    setLoading(true);
+
+    try {
+      await uploadXML(upload);
+      console.log(upload);
+      setSnackbarMessage("File uploaded");
+    } catch (error) {
+      showSnackbar("Error processing files", "red");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <h1 className="title">XML Upload</h1>
@@ -30,10 +88,51 @@ export default function Upload() {
           },
         }}
       >
-        <Button className="new" component="label" variant="contained">
+        <Button
+          className="new"
+          component="label"
+          variant="contained"
+          startIcon={<CloudUploadRounded />}
+        >
           Upload
+          <input
+            type="file"
+            name="upload-file"
+            hidden
+            onChange={handleFiles}
+            accept=".xml"
+          />
         </Button>
       </Box>
+      <Button
+        className="submit"
+        variant="contained"
+        onClick={handleSubmit}
+        disabled={loading}
+        sx={{
+          "&.Mui-disabled": {
+            backgroundColor: "#5e7889",
+            color: "white",
+          },
+        }}
+      >
+        {loading ? (
+          <CircularProgress size={24} sx={{ color: "white" }} />
+        ) : (
+          "Submit"
+        )}
+      </Button>
+      <Snackbar
+        open={snackbarOpen}
+        onClose={handleCloseSnackbar}
+        message={snackbarMessage}
+        autoHideDuration={5000}
+        sx={{
+          "& .MuiSnackbarContent-root": {
+            backgroundColor: snackbarColor,
+          },
+        }}
+      />
     </>
   );
 }
